375091871a169cad42c38c139a74ba9dcd269956
Marcos <mpalomvi8@alumnes.ub.edu>
2020-12-18 22:51:57 +0100

    prac4 medio hecha

--- prac4.c
+++ prac4.c
@@ -4,6 +4,47 @@
 #include "pracFuns.h"
 
 int main(int argc, char *argv[]){
+    int n; 
+    double interval1; 
+    double interval2; 
+    double tol; 
+    char nom_fitxer_entrada[80], nom_fitxer_sort[80]; 
+    FILE *fitxer_entrada;
+    FILE *fitxer_sortida;
+
+    printf("Donam els extrems del interval:\n");
+    scanf("%lf %lf", &interval1, &interval2);
+
+    printf("Dona'm el grau del polinomi:\n");
+    scanf("%d", &n);
+
+
+    printf("Escriu la tolerància:\n");
+    scanf("%le", &tol);
+
+
+    printf("Dona'm el nom del fitxer que vols obrir:\n");
+    scanf("%s", nom_fitxer_entrada);
+
+
+    fitxer_entrada = fopen(nom_fitxer_entrada, "r");
+
+    if (fitxer_entrada == NULL){
+        printf("Error al obrir el fitxer\n");
+        exit(1);
+    }
+
+    printf("Dona'm el nom del fitxer on escriurem els punts:\n");
+    scanf("%s", nom_fitxer_sort);
+    fitxer_sortida = fopen(nom_fitxer_sort, "w");
+
+    if (fitxer_sortida == NULL){
+        printf("Error al obrir el fitxer\n");
+        exit(2);
+    }
+
+    
+
 
 
 }
---
+++ prob20.gnu
@@ -0,0 +1,12 @@
+
+set size square
+set xlabel "x"
+set ylabel "y"
+#plot 'out5.res' w d tit 'polinomi' , 'exempl5.dat' notit, 'exempl5.spl' w d tit 'spline'
+#plot 'out5.res' w p pt 7 ps 0.1 tit 'polinomi' , 'exempl5.dat' notit, 'exempl5.spl' w p pt 7 ps 0.1 tit 'spline'
+plot 'prob20.pts' pt 6 tit "punts" , x*log(x) w l lt rgb "green" tit 'f(x)' , 'prob20.spl' w l lt rgb "red" tit 'spline'
+
+set key center bot
+set term jpeg size 800,800
+set outp 'prob20.jpg'
+replot
